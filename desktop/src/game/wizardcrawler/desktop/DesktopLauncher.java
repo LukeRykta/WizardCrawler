b10f1badd749db84dc908621b69304e733644071
LukeRykta <80142097+LukeRykta@users.noreply.github.com>
2021-04-20 16:58:07 -0500

    Add files via upload
    setup files for application

---
+++ desktop/src/game/wizardcrawler/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package game.wizardcrawler.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import game.wizardcrawler.WizardCrawlerApp;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new WizardCrawlerApp(), config);
+	}
+}
