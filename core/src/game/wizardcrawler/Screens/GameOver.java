5a2f881c06b057a020717b0b7c6a993642925616
thepositivepiet <59098232+thepositivepiet@users.noreply.github.com>
2021-05-07 15:55:29 -0500

    added simple game over screen with score at top.

--- core/src/game/wizardcrawler/Screens/GameOver.java
+++ core/src/game/wizardcrawler/Screens/GameOver.java
@@ -7,14 +7,20 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import game.wizardcrawler.Scenes.Hud;
 import game.wizardcrawler.WizardCrawlerApp;
+
+import static game.wizardcrawler.Scenes.Hud.score;
 
 public class GameOver implements Screen {
     private Viewport viewport;
@@ -24,41 +30,23 @@ public class GameOver implements Screen {
 
     public GameOver(Game game){
         this.game = game;
-        viewport = new FitViewport(WizardCrawlerApp.V_WIDTH, WizardCrawlerApp.V_HEIGHT, new OrthographicCamera());
+        viewport = new StretchViewport(WizardCrawlerApp.V_WIDTH, WizardCrawlerApp.V_HEIGHT, new OrthographicCamera());
         stage = new Stage(viewport, ((WizardCrawlerApp) game).batch);
+
+        Table testTable = new Table();
+        testTable.setBackground(new TextureRegionDrawable(new TextureRegion(new Texture("Backgrounds/game-over.jpg"))));
+        testTable.setFillParent(true);
+        stage.addActor(testTable);
 
         Label.LabelStyle font = new Label.LabelStyle(new BitmapFont(), Color.WHITE);
 
-        Table table = new Table();
-        table.center();
-        table.setFillParent(true);
+        testTable.top();
 
-        Label gameOverLabel = new Label("GAME OVER", font);
-        table.add(gameOverLabel).expandX();
-
-        stage.addActor(table);
-
-        Label.LabelStyle fontSpace = new Label.LabelStyle(new BitmapFont(), Color.WHITE);
-
-        Table tableTwo = new Table();
-        tableTwo.bottom();
-        tableTwo.setFillParent(true);
-
-        Label gameOverSpace = new Label("Press Space to Exit", fontSpace);
-        tableTwo.add(gameOverSpace).expandX();
-
-        stage.addActor(tableTwo);
-
-        Label.LabelStyle fontEnter = new Label.LabelStyle(new BitmapFont(), Color.WHITE);
-
-        Table tableThree = new Table();
-        tableThree.top();
-        tableThree.setFillParent(true);
-
-        Label gameOverEnter = new Label("Press Enter to Restart", fontEnter);
-        tableThree.add(gameOverEnter).expandX();
-
-        stage.addActor(tableThree);
+        if (score == null) {
+            score = 0;
+        }
+        Label gameOverLabel = new Label("Score: " + score, font);
+        testTable.add(gameOverLabel).expandX();
 
     }
 
@@ -81,6 +69,7 @@ public class GameOver implements Screen {
         update();
         Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act();
         stage.draw();
     }
 
