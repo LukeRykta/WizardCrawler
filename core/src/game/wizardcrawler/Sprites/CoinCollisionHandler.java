b7315988b09f2f6891f722eaa52be1b166dfb865
LukeRykta <nzp744@my.utsa.edu>
2021-05-07 22:31:04 -0500

    changed the behavior of items on the map to get picked up when you pass by them

--- core/src/game/wizardcrawler/Sprites/CoinCollisionHandler.java
+++ core/src/game/wizardcrawler/Sprites/CoinCollisionHandler.java
@@ -1,7 +1,9 @@
 package game.wizardcrawler.Sprites;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.math.Rectangle;
+import game.wizardcrawler.Scenes.Hud;
 import game.wizardcrawler.Screens.Play;
 import game.wizardcrawler.WizardCrawlerApp;
 
@@ -16,7 +18,9 @@ public class CoinCollisionHandler extends InteractiveTileObject{
     public void onHeadHit() {
         Gdx.app.log("Coin", "Collision detected");
         getCell().setTile(null);
-        //setCategoryFilter(WizardCrawlerApp.EMPTYCOIN_BIT);
+        setCategoryFilter(WizardCrawlerApp.ACCESSED_BIT);
+        Hud.addScore(100);
+        WizardCrawlerApp.manager.get("Audio/Sounds/pickaxe.mp3", Sound.class).play();
     }
 
     @Override
